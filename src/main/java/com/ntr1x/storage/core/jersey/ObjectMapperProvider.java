0b930f9d051c2706bb866746d9ade842b65d1d79
Igor Pavlenko <ipavlenko@inbox.ru>
2017-04-24 21:48:31 +0500

    Full data for derived classes

--- src/main/java/com/ntr1x/storage/core/jersey/ObjectMapperProvider.java
+++ src/main/java/com/ntr1x/storage/core/jersey/ObjectMapperProvider.java
@@ -1,41 +1,50 @@
-package com.ntr1x.storage.core.jersey;
-
-import javax.ws.rs.ext.ContextResolver;
-import javax.ws.rs.ext.Provider;
-
-import org.springframework.stereotype.Component;
-
-import com.fasterxml.jackson.databind.AnnotationIntrospector;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
-import com.fasterxml.jackson.databind.type.TypeFactory;
-import com.fasterxml.jackson.module.jaxb.JaxbAnnotationIntrospector;
-
-@Provider
-@Component
-public class ObjectMapperProvider  implements ContextResolver<ObjectMapper> {
-
-    private final ObjectMapper objectMapper;
-
-    public ObjectMapperProvider() {
-        
-    	objectMapper = new ObjectMapper()
-            .configure(SerializationFeature.WRAP_ROOT_VALUE, true)
-            .configure(DeserializationFeature.UNWRAP_ROOT_VALUE, true)
-            .enable(SerializationFeature.INDENT_OUTPUT)
-            .setAnnotationIntrospector(
-        		AnnotationIntrospector.pair(
-            		new JacksonAnnotationIntrospector(),
-            		new JaxbAnnotationIntrospector(TypeFactory.defaultInstance())
-        		)
-    		)
-        ;
-    }
-
-    @Override
-    public ObjectMapper getContext(final Class<?> type) {
-    	return objectMapper;
-    }
-}
+//package com.ntr1x.storage.core.jersey;
+//
+//import java.util.Set;
+//
+//import javax.ws.rs.ext.ContextResolver;
+//import javax.ws.rs.ext.Provider;
+//
+//import org.reflections.Reflections;
+//import org.springframework.stereotype.Component;
+//
+//import com.fasterxml.jackson.databind.AnnotationIntrospector;
+//import com.fasterxml.jackson.databind.DeserializationFeature;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.databind.SerializationFeature;
+//import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
+//import com.fasterxml.jackson.databind.type.TypeFactory;
+//import com.fasterxml.jackson.module.jaxb.JaxbAnnotationIntrospector;
+//import com.ntr1x.storage.core.model.Resource;
+//
+//@Provider
+//@Component
+//public class ObjectMapperProvider  implements ContextResolver<ObjectMapper> {
+//
+//    private final ObjectMapper objectMapper;
+//
+//    public ObjectMapperProvider() {
+//        
+//    	objectMapper = new ObjectMapper()
+//            .configure(SerializationFeature.WRAP_ROOT_VALUE, true)
+//            .configure(DeserializationFeature.UNWRAP_ROOT_VALUE, true)
+//            .enable(SerializationFeature.INDENT_OUTPUT)
+//            .enableDefaultTyping()
+//            .setAnnotationIntrospector(
+//        		AnnotationIntrospector.pair(
+//            		new JacksonAnnotationIntrospector(),
+//            		new JaxbAnnotationIntrospector(TypeFactory.defaultInstance())
+//        		)
+//    		)
+//        ;
+//    	
+//    	Reflections reflections = new Reflections("com.ntr1x.storage");
+//    	Set<Class<? extends Resource>> resourceSubTypes = reflections.getSubTypesOf(Resource.class);
+//    	objectMapper.registerSubtypes(resourceSubTypes.toArray(new Class<?>[resourceSubTypes.size()]));
+//    }
+//
+//    @Override
+//    public ObjectMapper getContext(final Class<?> type) {
+//    	return objectMapper;
+//    }
+//}
